ff23ec5b8a4904a6d02c0db203e7ea7f87823df0
Adarsh Thapa <realadarshthapa@gmail.com>
2020-05-10 11:25:27 +0545

    added game arena

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 bool gameOver;
 
-const int WIDTH = 36;
-const int HEIGHT = 36;
+const int WIDTH = 20;
+const int HEIGHT = 20;
 
 int x, y; // position of the player
 int foodX, foodY; // position of the food
@@ -21,16 +22,41 @@ void settings() {
     x = WIDTH / 2; 
     y = HEIGHT / 2;
     
-    // helps to span our food in the random (0 to 35: on x and y) location on the arena
+    // helps to span our food in the random (0 to 19: on x and y) location on the arena
     foodX = rand() % WIDTH;
     foodY = rand() % HEIGHT;
     
     score = 0;
-    
 }
 
 void visual() {
+    system("clear");
     
+    // creates top walls
+    for (int i = 0; i < WIDTH+2; i++) { // +2 for remaining two # on the top wall of the arena
+        cout << "#";
+    }
+    
+    cout << endl;
+    
+    // creates side walls
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (j == 0)
+                cout << "#";
+                cout << " ";
+                if (j == WIDTH-1)
+                    cout << "#";
+        }
+        cout << endl;
+    }
+    
+    // creates bottom walls
+    for (int i = 0; i < WIDTH+2; i++) { // +2 for remaining two # on the bottom wall of the arena
+        cout << "#";
+    }
+    
+    cout << endl;
 }
 
 void logic() {
@@ -45,9 +71,9 @@ int main() {
     settings();
     
     while (!gameOver) {
-        visual()
-        input()
-        logic()
+        visual();
+        input();
+        logic();
     }
     
     return 0;
